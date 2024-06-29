a6ffbf24d20025f14c1bfe057bd79b4af6e94d5a
Pratyay Paul <pratyaydocker@gmail.com>
2024-06-29 10:53:11 -0400

    Updated code and deleted the ShopApp Class

--- src/main/java/com/syntexpro/Customer.java
+++ src/main/java/com/syntexpro/Customer.java
@@ -24,5 +24,33 @@ public class Customer {
         this.size = size;
     }
 
+    public static void setSizeBasedOnMeasurement(Customer customer, int measurement) {
+        switch (measurement) {
+            case 1:
+                customer.setSize("S");
+                break;
+            case 2:
+                customer.setSize("M");
+                break;
+            case 3:
+                customer.setSize("L");
+                break;
+            default:
+                customer.setSize("X");
+                break;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Customer c1 = new Customer();
+
+        c1.setName("John");
+
+        int measurement = 3;        // Setting size of the customer based on the measurement value
+
+        System.out.println("Hello, " + c1.getName() + "!");     // Greeting message
+    }
+
 
 }
