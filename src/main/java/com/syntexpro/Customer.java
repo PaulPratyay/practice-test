68b7db04148f977ab9c8c4844d9bd8c5dcd8b3ed
Pratyay Paul <pratyaydocker@gmail.com>
2024-06-29 10:27:06 -0400

    Updated second part of the code

--- src/main/java/com/syntexpro/Customer.java
+++ src/main/java/com/syntexpro/Customer.java
@@ -3,6 +3,7 @@ package com.syntexpro;
 public class Customer {
 
     private String name;
+    private String size;
 
     public String getName() {
         return name;
@@ -10,6 +11,14 @@ public class Customer {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
     }
 
 
